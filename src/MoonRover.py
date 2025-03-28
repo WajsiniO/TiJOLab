9218525c330a3930fff26ff37ce1b385098e4465
WajsiniO <alekswajs@interia.pl>
2025-03-28 14:43:15 +0100

    finished
    all working

--- src/MoonRover.py
+++ src/MoonRover.py
@@ -8,4 +8,69 @@ class MoonRover(Vehicle):
         self.angle = 0
 
     def rotate(self, rotation):
+        if rotation is None:
+            return None
         self.angle += rotation
+
+    def reset(self):
+        self.angle = 0
+
+    def driveForward(self, distance):
+        if distance is None:
+            return None
+
+        if self.angle == -360:
+            self.reset()
+
+        if self.angle == 0:
+            self.Y += distance
+        elif self.angle == 180 or self.angle == -180:
+            self.Y -= distance
+        elif self.angle == 90 or self.angle == -270:
+            self.X += distance
+        elif self.angle == 270 or self.angle == -90:
+            self.X -= distance
+
+    def driveBackward(self, distance):
+
+        if distance is None:
+            return None
+
+        if self.angle == -360:
+            self.reset()
+
+        if self.angle == 0:
+            self.Y -= distance
+        elif self.angle == 180 or self.angle == -180:
+            self.Y += distance
+        elif self.angle == 90 or self.angle == -270:
+            self.X -= distance
+        elif self.angle == 270 or self.angle == -90:
+            self.X += distance
+
+    def localPoint(self):
+        if self.angle == 0:
+            zwrot = "North"
+        elif self.angle == 90 or self.angle == -270:
+            zwrot = "East"
+        elif self.angle == 180 or self.angle == -180:
+            zwrot = "South"
+        elif self.angle == 270 or self.angle == -90:
+            zwrot = "West"
+        else :
+            zwrot = "None"
+        print(self.Y, self.X, zwrot)
+
+
+
+
+def main():
+    krzys = MoonRover(0, 0)
+    krzys.driveForward(10)
+    krzys.localPoint()
+    krzys.rotate(90)
+    krzys.driveBackward(30)
+    krzys.localPoint()
+
+if __name__ == "__main__":
+    main()
--- test/moon_rover_test.py
+++ test/moon_rover_test.py
@@ -22,5 +22,72 @@ class testMoonRover(unittest.TestCase):
         tester = MoonRover(2, 1)
         self.assertIsNone(tester.rotate(None))
 
+    def test_is_rotation_angle_Not_None(self):
+        tester = MoonRover(2, 1)
+        tester.rotate(90)
+        self.assertEqual(tester.angle, 90)
+
+    def test_is_drive_forward_value_None(self):
+        tester = MoonRover(0,0)
+        self.assertIsNone(tester.driveForward(None), None)
+
+    def test_is_drive_forward_value_Not_None(self):
+        tester = MoonRover(0,0)
+        tester.driveForward(1)
+        self.assertEqual(tester.Y, 1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_Right(self):
+        tester = MoonRover(0,0)
+        tester.rotate(90)
+        tester.driveForward(1)
+        self.assertEqual(tester.X, 1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_Right_on_minus(self):
+        tester = MoonRover(0,0)
+        tester.rotate(-270)
+        tester.driveForward(1)
+        self.assertEqual(tester.X, 1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_to_back(self):
+        tester = MoonRover(0,0)
+        tester.rotate(180)
+        tester.driveForward(1)
+        self.assertEqual(tester.Y, -1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_to_back_on_minus(self):
+        tester = MoonRover(0,0)
+        tester.rotate(-180)
+        tester.driveForward(1)
+        self.assertEqual(tester.Y, -1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_Left(self):
+        tester = MoonRover(0,0)
+        tester.rotate(270)
+        tester.driveForward(1)
+        self.assertEqual(tester.X, -1)
+
+    def test_is_drive_forward_value_Not_None_Rotated_Left_on_minus(self):
+        tester = MoonRover(0,0)
+        tester.rotate(-90)
+        tester.driveForward(1)
+        self.assertEqual(tester.X, -1)
+
+    def test_is_drive_backward_value_None(self):
+        tester = MoonRover(0, 0)
+        self.assertIsNone(tester.driveBackward(None), None)
+
+    def test_is_drive_backward_value_Not_None(self):
+        tester = MoonRover(0, 0)
+        tester.driveBackward(1)
+        self.assertEqual(tester.Y, -1)
+
+    def test_is_drive_backward_value_Not_None_Rotated(self):
+        tester = MoonRover(0, 0)
+        tester.rotate(90)
+        tester.driveBackward(1)
+        self.assertEqual(tester.X, -1)
+
+
+
 if __name__ == '__main__':
     unittest.main()
