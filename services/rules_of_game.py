0b1bc1f87e9b222c9a5f7eff17cf777726e01f4b
WajsiniO <alekswajs@interia.pl>
2025-04-25 09:54:09 +0200

    wpis plików

---
+++ services/rules_of_game.py
@@ -0,0 +1,26 @@
+class RulesOfGame:
+
+    """
+        Metoda zwraca true, tylko gdy przejscie z polozenia source na destination w jednym ruchu jest zgodne
+        z zasadami gry w szachy.
+    """
+    def is_correct_move(self, source, destination):
+        raise NotImplementedError("Subclasses must implement this method")
+
+class Bishop(RulesOfGame):
+    def is_correct_move(self, source, destination):
+        if source is None or destination is None:
+            return False
+        source_col, source_row = source
+        dest_col, dest_row = destination
+
+        return abs(source_col - dest_col) == abs(source_row - dest_row) and source != destination
+
+class Knight(RulesOfGame):
+    def is_correct_move(self, source, destination):
+        # TODO: Prosze dokonczyc implementacje
+
+        return True
+
+# TODO: Prosze dokonczyc implementacje kolejnych figur szachowych: Knight, King, Queen, Rook, Pawn
+# TODO: Klasy powinny dziedziczyc RulesOfGame
